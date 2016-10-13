5afbc675f685d84cf120ee1551a6c949f797f6bb
Jerry Hu <hh201020@gmail.com>
2016-10-13 11:39:13 -0400

    Spring annotation configuration by using XML

--- src/com/pluralsight/repository/HibernatecustomerRepositoryImpl.java
+++ src/com/pluralsight/repository/HibernatecustomerRepositoryImpl.java
@@ -3,9 +3,16 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
 import com.pluralsight.model.Customer;
 
+@Repository("customerRepository")
 public class HibernatecustomerRepositoryImpl implements CustomerRepository {
+	
+	@Value("${someProperty}")
+	private String someValue = "test"; //No use to define a default value here;
 	/* (non-Javadoc)
 	 * @see com.pluralsight.repository.CustomerRepository#findAll()
 	 */
@@ -13,7 +20,8 @@ public class HibernatecustomerRepositoryImpl implements CustomerRepository {
 	public List<Customer> findAll(){
 		List<Customer> customers = new ArrayList<>();
 		Customer customer = new Customer();
-		customer.setFirstname("sds");
+		customer.setFirstname(someValue);
+//		customer.setFirstname("sds");
 		customer.setLastname("HH");
 		
 		customers.add(customer);
